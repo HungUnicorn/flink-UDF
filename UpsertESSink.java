1bed5a81cc86010db1ec5c839ad03054479853cb
HungUnicorn <unicorn.banachi@gmail.com>
2017-04-17 15:36:24 +0200

    Upsert ES Sink

---
+++ UpsertESSink.java
@@ -0,0 +1,62 @@
+import org.apache.flink.api.common.functions.RuntimeContext;
+import org.apache.flink.shaded.com.google.common.collect.Maps;
+import org.apache.log4j.Logger;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.client.Requests;
+import org.json.simple.JSONObject;
+
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ES upsert requests
+ *
+ */
+public class UpserESSink implements Serializable {
+
+    private Map<String, String> config = Maps.newHashMap();
+    private String elasticsearch;
+
+    public ESUpsertRequestEventCounts(String elasticsearch) {
+        // Best bulk size: 5-15 MB
+        // Ref: https://www.elastic.co/guide/en/elasticsearch/guide/current/bulk.html
+        config.put("bulk.flush.max.size.mb", "50");
+
+        config.put("bulk.flush.interval.ms", "10000");
+        config.put("cluster.name", "YOUR_ES_NAME");
+        this.elasticsearch = elasticsearch;
+    }
+
+    // Transport client
+    public ElasticsearchSink elasticsearchSink(){
+
+        List<InetSocketAddress> transports = new ArrayList<InetSocketAddress>();
+        transports.add(new InetSocketAddress(this.elasticsearch, 9300));
+        return new ElasticsearchSink<>(config, transports, new ElasticsearchSinkFunction<JSONObject>() {
+            @Override
+            public void process(JSONObject element, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
+            }
+        });
+    }
+
+    public static UpdateRequest updateRequest (JSONObject element) {
+      // If doc doens't exist, upsert() is used, if doc exists, updateRequest is used
+      String index = "Your_ES_INDEX"
+      String type = "YOUR_ES_TYPE"
+      docId = String.valueOf(element.get("id"))
+        return new UpdateRequest()
+                .index(index)
+                .type(type)
+                .id(docId)
+                .doc(element)
+                .upsert(Requests.indexRequest()
+                        .index(index)
+                        .type(type)
+                        .id(docId)
+                        .source(element));
+
+    }
+}
